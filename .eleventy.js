58c040d964929887ed03edce5eb231a7bb15a43e
Matt Feifarek <matt.feifarek@gmail.com>
2020-04-30 16:46:25 -0500

    Eleventy Installed and configured, readme in the docs dir

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+
+  // get static files into the publish-able root
+  eleventyConfig.addPassthroughCopy({"source/_static/*.*": "/"});
+  eleventyConfig.addPassthroughCopy({"source/_static/img": "img"});
+  eleventyConfig.addPassthroughCopy({"source/_static/css": "css"});
+  eleventyConfig.addPassthroughCopy({"source/_static/js": "js"});
+  //console.log(eleventyConfig);
+
+  return {
+    dir: {
+      input: "source",
+      output: "docs",
+      includes: "_layouts",
+      layouts: "_layouts",
+    },
+  };
+};
