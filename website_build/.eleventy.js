e62250f8e10aac605615ac840fc2cf84079dd53e
Matt Feifarek <matt.feifarek@gmail.com>
2020-05-12 16:11:04 -0500

    shortcodes for social icons/links

--- website_build/.eleventy.js
+++ website_build/.eleventy.js
@@ -1,5 +1,18 @@
-module.exports = function(eleventyConfig) {
 
+function icon (name, size = 32) {
+  // size is from Bulma (here: https://bulma.io/documentation/elements/image/)
+  // and can be 16 24 32 48 64 96 128
+  return `<figure class="image is-${size}x${size}"><img src="/img/icons/${name}.svg" alt="${name} icon"></figure>`
+};
+
+function social_link_icon(link, clss = "", size = 32) {
+  // send in css class string for clss
+  const look_for = ["facebook", "instagram", "linkedin", "twitter", "email"];
+  const name = look_for.find(n => link.toLowerCase().includes(n));
+  return `<a href="${link}" target="_blank" class="${clss}" title="navigate to ${name}...">${icon(name, size)}</a>`
+};
+
+module.exports = function (eleventyConfig) {
   // get static files into the publish-able root
   const static_files = ['robots.txt', 'README.md', 'CNAME'];
   static_files.forEach(sf => {
@@ -16,6 +29,14 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"static/css": "css"});
   eleventyConfig.addPassthroughCopy({"static/js": "js"});
   //console.log(eleventyConfig);
+
+  // 'shortcodes' for using in templates
+
+  // icon: use {% icon 'facebook' 64 %} for example
+  eleventyConfig.addShortcode("icon", icon);
+
+  // social_link_icon: use {% isocial_link_icon 'http://facebook.com/something_real_here' "myCssClasses" 64 %} for example
+  eleventyConfig.addShortcode("social_link_icon", social_link_icon);
 
   return {
     dir: {
